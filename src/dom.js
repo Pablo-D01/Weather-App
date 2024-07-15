989c263d646f4d899f44335aaa8185902d1b92ff
Paweł D <you@example.com>
2024-07-15 15:20:04 +0200

    styling

--- src/dom.js
+++ src/dom.js
@@ -3,8 +3,20 @@ function displayWeather(data) {
   const currentConditions = data.currentConditions;
   const country = data.resolvedAddress.split(", ").pop();
 
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  const currentDay = new Date(data.days[0].datetime).getDay();
   const currentHtml = `
       <h2>Weather in ${data.address}, ${country}</h2>
+      <p>${daysOfWeek[currentDay]}</p>
       <p>Current Temperature: ${currentConditions.temp} °C</p>
       <p>Current Condition: ${currentConditions.conditions}</p>
   `;
@@ -12,9 +24,12 @@ function displayWeather(data) {
   const forecastHtml = data.days
     .slice(1, 8)
     .map((day) => {
+      const dayOfWeek = new Date(day.datetime).getDay();
       return `
           <div class="forecast-day">
-              <h3>${new Date(day.datetime).toLocaleDateString()}</h3>
+              <h3>${daysOfWeek[dayOfWeek]} - ${new Date(
+        day.datetime
+      ).toLocaleDateString()}</h3>
               <p>Temperature: ${day.temp} °C</p>
               <p>Condition: ${day.conditions}</p>
               <p>Chance of rain: ${day.precipprob}%</p>
